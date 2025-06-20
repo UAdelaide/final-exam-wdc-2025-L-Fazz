9ee0643d11dd0eef9f750e1a33c360908f223a58
Lario <a1853413@adelaide.edu.au>
2025-06-20 04:42:13 +0000

    [automatic commit] 2025-06-20 04:42:13 UTC 14.2.87.76

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../db');
-const seed = require('./seed')
 
 // GET /api/dogs
 router.get('/dogs', async (req, res) => {
