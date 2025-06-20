1a0d70a1233ee0bdf5d1848dd4cf1951ef3ef440
Lario <a1853413@adelaide.edu.au>
2025-06-20 04:36:00 +0000

    [automatic commit] 2025-06-20 04:36:00 UTC 14.2.87.76

---
+++ part1/app.js
@@ -0,0 +1,18 @@
+// app.js
+const express = require('express');
+const pool = require('./db');
+const seed = require('./seed');
+const apiRoutes = require('./routes/api');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use('/api', apiRoutes);
+
+// Seed data on startup
+seed(pool);
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
+});
